4a3496ef9a5ce8b3ad2c5741005a8995873e3a33
QUANG-SanBay <quangsanbay@gmail.com>
2025-07-31 13:50:20 +0700

    fix error: <w> Deprecation The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0. <w> <w> More info: https://sass-lang.com/d/legacy-js-api <w> <w> null

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,28 @@
 /* config-overrides.js */
 
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+  // Configure Sass loader to silence deprecation warnings
+  const oneOfRule = config.module.rules.find(rule => rule.oneOf);
+  
+  if (oneOfRule) {
+    oneOfRule.oneOf.forEach(rule => {
+      if (rule.test && rule.test.toString().includes('scss|sass')) {
+        if (rule.use && Array.isArray(rule.use)) {
+          rule.use.forEach(loader => {
+            if (loader.loader && loader.loader.includes('sass-loader')) {
+              if (!loader.options) {
+                loader.options = {};
+              }
+              if (!loader.options.sassOptions) {
+                loader.options.sassOptions = {};
+              }
+              loader.options.sassOptions.silenceDeprecations = ['legacy-js-api'];
+            }
+          });
+        }
+      }
+    });
+  }
+
   return config;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import GlobalStyles from './globalStyles'
+import GlobalStyles from './assets/css/globalStyles'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
