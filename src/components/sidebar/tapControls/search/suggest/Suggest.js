0671c200a2bb5e4bf05bd163c226b780e4532762
QUANG-SanBay <quangsanbay@gmail.com>
2025-08-21 15:04:26 +0700

    use tippy headless handle btn search. Then css for component Search.

--- src/components/sidebar/tapControls/search/suggest/Suggest.js
+++ src/components/sidebar/tapControls/search/suggest/Suggest.js
@@ -1,20 +1,35 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Suggest.module.scss';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Suggest() {
+    const [suggestResult, setSuggestResult] = useState([])
+    useEffect(() => {
+        setTimeout(() => {
+            setSuggestResult(
+                [
+                    1,
+                    2,
+                    3
+                ]
+            )
+        }, 3000)
+    }, [])
     return (
         <div className={styles.suggestCtn}>
             <div className={styles.suggestTitle}>You may like</div>
             <ul className={styles.suggestList}>
-                <li className={styles.suggestItem}>
-                    <div className={styles.suggestIcon}>
-                        <FontAwesomeIcon icon={faCircle} />
-                    </div>
-                    <div className={styles.suggestText}>
-                        <h4>Suggest</h4>
-                    </div>
-                </li>
+                {suggestResult.map(() => (
+                    <li className={styles.suggestItem}>
+                        <div className={styles.suggestIcon}>
+                            <FontAwesomeIcon icon={faCircle} />
+                        </div>
+                        <div className={styles.suggestText}>
+                            <h4>Suggest</h4>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     );
